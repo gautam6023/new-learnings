240d69b6fe862c4b7731f2b8f14f8041ff9c10f6
Gautam Gohil <gautamgohil0@gmail.com>
2022-10-16 02:02:58 +0530

    Started Learning NextJS

---
+++ NextJS/coffie-shop/pages/footer.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+import styles from "../styles/Footer.module.css";
+const Footer: NextPage = () => {
+  return <div className={styles.container}>Footer</div>;
+};
+
+export default Footer;
---
+++ NextJS/coffie-shop/pages/hello.tsx
@@ -0,0 +1,5 @@
+const hello = () => {
+  return <div>Hello World</div>;
+};
+
+export default hello;
